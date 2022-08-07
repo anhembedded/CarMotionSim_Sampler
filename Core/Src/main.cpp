fbfaa3d101966356e0849256b13dcde0f90a2352
Nathan Embedded <100819474+anhembedded@users.noreply.github.com>
2022-08-08 00:21:04 +0700

    Update main.cpp
    For readable

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -98,47 +98,6 @@ simtool getData(std::array<uint8_t, 12> data)
   return temp;
 }
 
-void getData(std::array<uint8_t, 10> &data, uint8_t &surge, uint8_t &sway, uint8_t &traction)
-{
-
-  constexpr auto nextByte = 1;
-  constexpr auto startByte = '[';
-
-  auto A_iter = std::find(data.begin(), data.end(), 'A');
-  auto B_iter = std::find(data.begin(), data.end(), 'B');
-  auto C_iter = std::find(data.begin(), data.end(), 'C');
-
-  auto exam = [data](decltype(A_iter) praIter)
-  {
-    if ((praIter != data.end()) && (praIter != data.begin()))
-    {
-      return 1;
-    }
-    return 0;
-  };
-
-  if (exam(A_iter))
-  {
-    if (*(A_iter - nextByte) == startByte)
-    {
-      surge = *(A_iter + nextByte);
-    }
-  }
-  if (exam(B_iter))
-  {
-    if (*(B_iter - nextByte) == startByte)
-    {
-      sway = *(B_iter + nextByte);
-    }
-  }
-  if (exam(C_iter))
-  {
-    if (*(C_iter - nextByte) == startByte)
-    {
-      traction = *(C_iter + nextByte);
-    }
-  }
-}
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config();
